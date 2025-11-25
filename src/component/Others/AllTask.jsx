ef307e5a341b9fdafc7638dd44d37bc5ecaf10ad
Mohd Abrar <abrarsheikh774@gmail.com>
2025-11-25 23:59:24 +0530

    Passinf Data in Task

--- src/component/Others/AllTask.jsx
+++ src/component/Others/AllTask.jsx
@@ -3,33 +3,26 @@ import { AuthContext } from '../Context/AuthProvider'
 
 const AllTask = () => {
   const  authData = useContext(AuthContext)
-  console.log(authData.employees)
+console.log(authData.employees)
   return (
-    <div className='tasklist bg-[#1C1C1C] p-5 rounded mt-5 h-48 overflow-auto'>
+    <div className='tasklist bg-[#1C1C1C] p-5 rounded mt-5  overflow-auto'>
       <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Abrar</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
+        <h2 className='w-1/5'>Employee Name</h2>
+        <h3 className='w-1/5'>New Task</h3>
+        <h5 className='w-1/5'>Active Task</h5>
+        <h5 className='w-1/5'>Completed</h5>
+        <h5 className='w-1/5'>Failed</h5>
         </div>
-        <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Abrar</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
+        <div>
+          {authData.employees.map(function(elem){
+            return <div className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2 className='w-1/5'>{elem.firstname}</h2>
+        <h3 className='w-1/5 text-blue-600'>Task</h3>
+        <h5 className='w-1/5 text-yellow-400'>Status</h5>
+        <h5 className='w-1/5 text-white'>Status</h5>
+        <h5 className='w-1/5 text-red-600'>Failed</h5>
         </div>
-        <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Abrar</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-        </div>
-        <div className='bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Abrar</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
-        </div>
-        <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2>Abrar</h2>
-        <h3>Make a UI design</h3>
-        <h5>Status</h5>
+          })}
         </div>
     </div>
   )
