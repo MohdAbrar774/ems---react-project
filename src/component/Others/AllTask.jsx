87ca9e892a7e2557e7a0d883fe4f9fd53e16209a
Mohd Abrar <abrarsheikh774@gmail.com>
2025-11-26 23:59:08 +0530

    Form Handler

--- src/component/Others/AllTask.jsx
+++ src/component/Others/AllTask.jsx
@@ -3,24 +3,23 @@ import { AuthContext } from '../Context/AuthProvider'
 
 const AllTask = () => {
   const  authData = useContext(AuthContext)
-console.log(authData.employees)
   return (
-    <div className='tasklist bg-[#1C1C1C] p-5 rounded mt-5  overflow-auto'>
+    <div className='tasklist bg-[#1C1C1C] p-5 rounded mt-5 '>
       <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2 className='w-1/5'>Employee Name</h2>
-        <h3 className='w-1/5'>New Task</h3>
-        <h5 className='w-1/5'>Active Task</h5>
-        <h5 className='w-1/5'>Completed</h5>
-        <h5 className='w-1/5'>Failed</h5>
+        <h2 className='text-lg font-medium w-1/5'>Employee Name</h2>
+        <h3 className='text-lg font-medium w-1/5'>New Task</h3>
+        <h5 className='text-lg font-medium w-1/5'>Active Task</h5>
+        <h5 className='text-lg font-medium w-1/5'>Completed</h5>
+        <h5 className='text-lg font-medium w-1/5'>Failed</h5>
         </div>
         <div>
-          {authData.employees.map(function(elem){
-            return <div className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
-        <h2 className='w-1/5'>{elem.firstname}</h2>
-        <h3 className='w-1/5 text-blue-600'>Task</h3>
-        <h5 className='w-1/5 text-yellow-400'>Status</h5>
-        <h5 className='w-1/5 text-white'>Status</h5>
-        <h5 className='w-1/5 text-red-600'>Failed</h5>
+          {authData.employees.map(function(elem,idx){
+            return <div key={idx} className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2 className='text-lg font-medium w-1/5'>{elem.firstname}</h2>
+        <h3 className='text-lg font-medium w-1/5 text-blue-600'>{elem.taskCount.active}</h3>
+        <h5 className='text-lg font-medium w-1/5 text-yellow-400'>{elem.taskCount.newTask}</h5>
+        <h5 className='text-lg font-medium w-1/5 text-white'>{elem.taskCount.completed}</h5>
+        <h5 className='text-lg font-medium w-1/5 text-red-600'>{elem.taskCount.failed}</h5>
         </div>
           })}
         </div>
